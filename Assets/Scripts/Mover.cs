c2d5f8e1263df2d76bdfe92281b691dc2ec82b19
inq39 <61730491+inq39@users.noreply.github.com>
2021-01-21 22:42:17 +0100

    terrain, navmesh

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    private NavMeshAgent _playerNavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerNavMeshAgent.SetDestination(_target.position);
+    }
+}
