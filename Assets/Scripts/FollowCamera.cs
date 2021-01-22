ba917103dd1f45ad7fdcb02de7dc7d2309a77088
inq39 <61730491+inq39@users.noreply.github.com>
2021-01-22 19:04:24 +0100

    import of new assets and animations

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+
+   
+    void Update()
+    {
+        transform.position = _target.position;
+    }
+}
