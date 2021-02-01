adcf448a2b5c49c2d6aa331c8eaf0f661580ebc0
inq39 <61730491+inq39@users.noreply.github.com>
2021-02-01 23:25:14 +0100

    second scene, portal, SceneManager

---
+++ Assets/Scripts/Core/Scene_Manager/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Island.Manager
+{
+    public class SceneController : MonoBehaviour
+    {
+       
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene("Prototype2_Island");
+            }
+        }
+    }
+}
